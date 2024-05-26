1105be271ab8b60ee486db720f5e66d995f130c1
Congxabeng103 <congdt04@gmail.com>
2024-05-26 12:24:55 +0700

    first commit

---
+++ src/main/java/com/poly/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.poly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+//	@Autowired
+//	ServletContext Application;
+//	@RequestMapping("/url.php")
+//	public String sayHello(HttpSession session, HttpServletRequest req,HttpServletResponse resp){
+//			String fullName = req.getParameter("hoten");
+//					req.setAttribute("message", "Http Components");
+//					return "hello";
+//	}
+	@RequestMapping("/index2")
+	public String index(Model model) {
+		model.addAttribute("message", "Welcome to Spring MVC! Welcome to Java5 Courseas!");
+		System.out.println("index2");
+		return "index2";
+
+}
+}
